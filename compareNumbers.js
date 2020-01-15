585fcae4e7688671f161f30bf48a403042f02a76
janelle mellor <janellemellor@gmail.com>
2020-01-14 20:34:15 -0800

    add testing

--- compareNumbers.js
+++ compareNumbers.js
@@ -1,7 +1,8 @@
-export default compareNumbers;
 
 const compareNumbers = (guess, correctNumber) => {
     if (guess === correctNumber) return 0;
     if (guess < correctNumber) return -1;
     if (guess > correctNumber) return 1;
 };
+
+export default compareNumbers;
--- test/example.test.js
+++ test/example.test.js
@@ -1,12 +1,23 @@
-import compareNumbers from 'compareNumbers.js';
+import compareNumbers from '../compareNumbers.js';
 
 const test = QUnit.test;
 
-test('Guess is correct', compareNumbers(assert){
-    const response = guess === correctNumber
-    const expect = true
+test('Guess is correct', function(assert){
+    const expect = 0;
+    const result = compareNumbers(3, 3);
+    assert.equal(result, expect);
+});
 
-    const result = compareNumbers 
-    
-    assert.equal(expect, result)
-} ;
+
+test('Guess is too low', function(assert){
+    const expect = -1;
+    const result = compareNumbers(1, 3);
+    assert.equal(result, expect);
+});
+
+
+test('Guess is too high', function(assert){
+    const expect = 1;
+    const result = compareNumbers(5, 3);
+    assert.equal(result, expect);
+});
